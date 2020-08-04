5601f47dfda75f332abbf2d66e3c265977192e0b
Farhanul Karim <farhanulkarim3770@gmail.com>
2020-08-04 02:00:07 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,5 +1,4 @@
-def second_challenge
-  epic_tragedy = {
+epic_tragedy = {
    :montague => {
       :patriarch => {},
       :matriarch => {},
@@ -13,5 +12,5 @@ def second_challenge
       :heroine_friends => []
    }
 }
-  
-end
+
+
